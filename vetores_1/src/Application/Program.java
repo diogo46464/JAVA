8baa4e9a3054d5ea2cb2c29ef5ff1b59f6cd8622
Diogo Pincetta <diogohe08@gmail.com>
2026-02-10 07:46:32 -0300

    Vetores 1 e 2

--- vetores_1/src/Application/Program.java
+++ vetores_1/src/Application/Program.java
@@ -12,23 +12,24 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		
 		double[] vect = new double[n];
 		
-		for(int i = 0; i <n; i++) {
+		for(int i = 0; i < n; i++) {
 			vect[i] = sc.nextDouble();
 		}
 		
-		
 		double sum = 0.0;
+		
 		for(int i = 0; i < n; i++) {
+			
 			sum += vect[i];
+			
 		}
 		
-		double avg = sum / n;
+		double avg = sum/n;
 		
-		System.out.printf("AVARAGE HEIGHT: %.2f%n" , avg);
-		
+		System.out.printf("AVERAGE EIGHT: %.2f%n", avg );
+
 		sc.close();
 		
 
